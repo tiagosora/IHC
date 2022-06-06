ee9d16d63586bebcbb798e0531342e8c5f5cb47e
Alberto Campos <tiagogcarvalho2002@gmail.com>
2022-06-06 14:58:41 +0100

    uiii

--- js/main.js
+++ js/main.js
@@ -196,3 +196,106 @@ function summarieUpdate(){
         document.getElementById("summaries_name").innerHTML = '<i class="fa-solid fa-book-open"></i>' + ' ' +  selected_option2 + ' SUMMARIES'
     }
 }
+
+function universitiesUpdate(){
+    var selected_option =  document.getElementById("area").value
+    document.getElementById("university1").style.display = "flex";
+    document.getElementById("university2").style.display = "flex";
+    document.getElementById("university3").style.display = "flex";
+    document.getElementById("university4").style.display = "flex";
+    document.getElementById("university5").style.display = "flex";
+    document.getElementById("university6").style.display = "flex";
+    document.getElementById("university7").style.display = "flex";
+    document.getElementById("university8").style.display = "flex";
+    document.getElementById("university9").style.display = "flex";
+    document.getElementById("university10").style.display = "flex";
+    if (selected_option == ""){
+        document.getElementById("uni1").innerHTML = 'Universidade de Lisboa : Engineering';
+        document.getElementById("uni2").innerHTML = 'Universidade do Porto : Medicine';
+        document.getElementById("uni3").innerHTML = 'Universidade do Porto : Engineering';
+        document.getElementById("uni4").innerHTML = 'Universidade do Minho : Medicine';
+        document.getElementById("uni5").innerHTML = 'Universidade de Aveiro : Engineering';
+        document.getElementById("uni6").innerHTML = 'Universidade do Porto : Humanities';
+        document.getElementById("uni7").innerHTML = 'Universidade de Coimbra : Medicine';
+        document.getElementById("uni8").innerHTML = 'Universidade Nova de Lisboa : Medicine';
+        document.getElementById("uni9").innerHTML = 'Universidade de Lisboa : Science';
+        document.getElementById("uni10").innerHTML = 'Universidade do Porto - Architecture';
+    }
+    if (selected_option == "Medicine"){
+        document.getElementById("uni1").innerHTML = 'Universidade do Porto';
+        document.getElementById("uni2").innerHTML = 'Universidade do Porto - 2';
+        document.getElementById("uni3").innerHTML = 'Universidade do Minho';
+        document.getElementById("uni4").innerHTML = 'Universidade de Coimbra';
+        document.getElementById("uni5").innerHTML = 'Universidade Nova de Lisboa';
+        document.getElementById("uni6").innerHTML = 'Universidade de Lisboa';
+        document.getElementById("uni7").innerHTML = 'Universidade da Beira Interior';
+        document.getElementById("uni8").innerHTML = 'Universidade dos Açores';
+        document.getElementById("uni9").innerHTML = 'Universidade da Madeira';
+        document.getElementById("university10").style.display = "none";
+    }
+    if (selected_option == "Education"){
+        document.getElementById("uni1").innerHTML = 'Universidade do Minho';
+        document.getElementById("uni2").innerHTML = 'Universidade do Porto';
+        document.getElementById("uni3").innerHTML = 'Universidade de Coimbra';
+        document.getElementById("uni4").innerHTML = 'Universidade de Lisboa';
+        document.getElementById("uni5").innerHTML = 'Universidade dos Açores';
+        document.getElementById("uni6").innerHTML = 'Universidade da Madeira';
+        document.getElementById("university7").style.display = "none";
+        document.getElementById("university8").style.display = "none";
+        document.getElementById("university9").style.display = "none";
+        document.getElementById("university10").style.display = "none";
+    }
+    if (selected_option == "Engineering"){
+        document.getElementById("uni1").innerHTML = 'Universidade de Aveiro';
+        document.getElementById("uni2").innerHTML = 'Universidade de Lisboa';
+        document.getElementById("uni3").innerHTML = 'Universidade do Porto';
+        document.getElementById("uni4").innerHTML = 'Instituto Politécnico do Porto';
+        document.getElementById("uni5").innerHTML = 'Universidade do Minho';
+        document.getElementById("uni6").innerHTML = 'Universidade de Coimbra';
+        document.getElementById("uni7").innerHTML = 'Universidade dos Açores';
+        document.getElementById("uni8").innerHTML = 'Universidade da Madeira';
+        document.getElementById("uni9").innerHTML = 'Universidade Católica';
+        document.getElementById("uni10").innerHTML = "Universidade Nova de Lisboa"
+    }
+    if (selected_option == "ss"){
+        document.getElementById("uni1").innerHTML = 
+        document.getElementById("uni2").innerHTML = 
+        document.getElementById("uni3").innerHTML = 
+        document.getElementById("uni4").innerHTML = 
+        document.getElementById("uni5").innerHTML = 
+        document.getElementById("uni6").innerHTML = 
+        document.getElementById("uni7").innerHTML = 
+        document.getElementById("uni8").innerHTML = 
+        document.getElementById("uni9").innerHTML = 
+        document.getElementById("uni10").innerHTML =
+        document.getElementById("university10").style.display = "none"
+    }
+    if (selected_option == "ss"){
+        document.getElementById("uni1").innerHTML = 
+        document.getElementById("uni2").innerHTML = 
+        document.getElementById("uni3").innerHTML = 
+        document.getElementById("uni4").innerHTML = 
+        document.getElementById("uni5").innerHTML = 
+        document.getElementById("uni6").innerHTML = 
+        document.getElementById("uni7").innerHTML = 
+        document.getElementById("uni8").innerHTML = 
+        document.getElementById("uni9").innerHTML = 
+        document.getElementById("uni10").innerHTML =
+        document.getElementById("university10").style.display = "none"
+    }
+    if (selected_option == "ss"){
+        document.getElementById("uni1").innerHTML = 
+        document.getElementById("uni2").innerHTML = 
+        document.getElementById("uni3").innerHTML = 
+        document.getElementById("uni4").innerHTML = 
+        document.getElementById("uni5").innerHTML = 
+        document.getElementById("uni6").innerHTML = 
+        document.getElementById("uni7").innerHTML = 
+        document.getElementById("uni8").innerHTML = 
+        document.getElementById("uni9").innerHTML = 
+        document.getElementById("uni10").innerHTML =
+        document.getElementById("university10").style.display = "none"
+    }
+    
+
+}
